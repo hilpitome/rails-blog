4be8bc87e4aa7c1e735b11a1e26816c4adb73049
hilpitome <hilpitome@gmail.com>
2015-08-24 17:41:05 +0300

    defined update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   def edit
   @article = Article.find(params[:id])
 end
- 
+
   def create
     @article = Article.new(article_params)
 
@@ -22,6 +22,14 @@ end
     else
       render 'new'
     end
+  end
+  def  update
+    @article = Article(.find(params[:id])
+
+     if @article.update(article_params)
+     redirect_to @article
+   else
+     render 'edit'
   end
   private
   def article_params
